a024649976121f01ff6f711d070b73e27523f094
lebedevv <sintrael@gmail.com>
2024-01-19 14:20:33 +0300

    Searching relative to buffer is bad?

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,14 +2,19 @@ local builtin = require('telescope.builtin')
 local utils = require('telescope.utils')
 
 vim.keymap.set('n', '<leader>pf', function()
-    builtin.find_files({show_untracked = false, cwd=utils.buffer_dir()})
+    builtin.find_files({
+        show_untracked = false, 
+        -- cwd=utils.buffer_dir()
+    })
 end)
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>pg', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set('n', '<leader>ps', function()
-    builtin.live_grep({cwd=utils.buffer_dir({cwd=utils.buffer_dir()})})
+    builtin.live_grep({
+        -- cwd=utils.buffer_dir()
+    })
 end)
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>gl', builtin.git_commits, {})
