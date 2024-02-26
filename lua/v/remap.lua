0b664b6b3f51da159584fc35771068941b08843e
lebedevv <sintrael@gmail.com>
2024-02-26 13:31:33 +0300

    paste instead of word under cursor, casting it into void register : <leader>pp in normal mode

--- lua/v/remap.lua
+++ lua/v/remap.lua
@@ -4,10 +4,10 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("n", "<leader>pp", [[viw"_dP]])
+-- vim.keymap.set("x", "<leader>pl", [[viw"_dP]])
 
--- next greatest remap ever : asbjornHaland
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
