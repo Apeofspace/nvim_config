4ed9cb3fab8087d460d835a060f5163217c45ec5
lebedevv <sintrael@gmail.com>
2024-06-04 15:07:53 +0300

    Cycles between colorchemes with <leader>cn, <leader>cp !!

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,3 +1,73 @@
+local colorschemes = {
+  'sonokai',
+  'monokai-pro',
+  'tokyonight-night',
+  'eldritch',
+  'cyberdream',
+  'catppuccin',
+  'melange',
+  'monet',
+  'hybrid',
+}
+
+-- Path to the file where the colorscheme will be saved
+local colorscheme_file = vim.fn.stdpath 'config' .. '/lua/custom/colorscheme.txt'
+local colorschemeindex = 1
+
+local function read_colorscheme()
+  local f = io.open(colorscheme_file, 'r')
+  if f then
+    local colorscheme = f:read '*n'
+    f:close()
+    return colorscheme
+  end
+  return nil
+end
+
+local function write_colorscheme(colorscheme)
+  local f = io.open(colorscheme_file, 'w')
+  if f then
+    f:write(colorscheme)
+    f:close()
+  end
+end
+
+local function NextColorScheme()
+  colorschemeindex = colorschemeindex + 1
+  if colorschemeindex <= 0 or colorschemeindex > #colorschemes then
+    colorschemeindex = 1
+  end
+  vim.cmd('colorscheme ' .. colorschemes[colorschemeindex])
+  vim.notify('Colorscheme: ' .. colorschemes[colorschemeindex])
+  write_colorscheme(colorschemeindex)
+end
+
+local function PrevColorScheme()
+  colorschemeindex = colorschemeindex - 1
+  if colorschemeindex <= 0 or colorschemeindex > #colorschemes then
+    colorschemeindex = #colorschemes
+  end
+  vim.cmd('colorscheme ' .. colorschemes[colorschemeindex])
+  vim.notify('Colorscheme: ' .. colorschemes[colorschemeindex])
+  write_colorscheme(colorschemeindex)
+end
+
+vim.keymap.set('n', '<leader>cn', NextColorScheme, { desc = '[N]ext [C]olorscheme' })
+vim.keymap.set('n', '<leader>cp', PrevColorScheme, { desc = '[P]rev [C]olorscheme' })
+
+-- Defer loading the colorscheme until VimEnter
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = function()
+    local saved_index = read_colorscheme()
+    if saved_index and saved_index >= 1 and saved_index <= #colorschemes then
+      colorschemeindex = saved_index
+      vim.cmd('colorscheme ' .. colorschemes[colorschemeindex])
+    else
+      vim.cmd('colorscheme ' .. colorschemes[1])
+    end
+  end,
+})
+
 return {
   {
     'sainnhe/sonokai',
@@ -66,6 +136,42 @@ return {
     config = function()
       -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
       -- vim.cmd.colorscheme 'melange'
+    end,
+  },
+  {
+    'fynnfluegge/monet.nvim',
+    name = 'monet',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('monet').setup {
+        transparent_background = false,
+        semantic_tokens = true,
+        dark_mode = true,
+        highlight_overrides = {},
+        color_overrides = {},
+        styles = {
+          strings = { 'italic' },
+          comments = { 'italic' },
+        },
+      }
+      -- vim.cmd.colorscheme 'monet'
+    end,
+  },
+  {
+    'HoNamDuong/hybrid.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('hybrid').setup {
+        italic = {
+          strings = true,
+          emphasis = true,
+          comments = true,
+          folds = true,
+        },
+      }
+      -- vim.cmd.colorscheme 'hybrid'
     end,
   },
 }
