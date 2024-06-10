e7fefaa0f88478a39dbbd8c957ef6ddd9e9a8da8
lebedevv <sintrael@gmail.com>
2024-06-10 10:00:54 +0300

    Better keys for harpoon! Ctrl+numbers. Also can switch in harpoon window with just numbers 1 to 9.
    Also disabled rnvimr because fuck it. use oil

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -112,35 +112,83 @@
 
 -- OLDE TELESCOPELESS CONFIG
 return {
-  'ThePrimeagen/harpoon',
-  branch = 'harpoon2',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  lazy = false,
-  config = function()
-    local harpoon = require 'harpoon'
-    -- REQUIRED
-    harpoon:setup()
-    -- REQUIRED
-    vim.keymap.set('n', '<leader>a', function()
-      harpoon:list():append()
-    end, { desc = 'Harpoon add' })
-    vim.keymap.set('n', '<leader>h', function()
-      harpoon.ui:toggle_quick_menu(require('harpoon'):list())
-    end, { desc = 'Harpoon list' })
-    vim.keymap.set('n', '<leader>q', function()
-      harpoon:list():select(1)
-    end, { desc = 'Harpoon file 1' })
-    vim.keymap.set('n', '<leader>w', function()
-      harpoon:list():select(2)
-    end, { desc = 'Harpoon file 2' })
-    vim.keymap.set('n', '<leader>e', function()
-      harpoon:list():select(3)
-    end, { desc = 'Harpoon file 3' })
-    vim.keymap.set('n', '<leader>r', function()
-      harpoon:list():select(4)
-    end, { desc = 'Harpoon file 4' })
-    vim.keymap.set('n', '<leader>t', function()
-      harpoon:list():select(5)
-    end, { desc = 'Harpoon file 5' })
-  end,
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	lazy = false,
+	config = function()
+		local harpoon = require("harpoon")
+		-- REQUIRED
+		harpoon:setup()
+		-- REQUIRED
+		vim.keymap.set("n", "<leader>a", function()
+			harpoon:list():append()
+		end, { desc = "Harpoon add" })
+		vim.keymap.set("n", "<leader>h", function()
+			harpoon.ui:toggle_quick_menu(require("harpoon"):list())
+		end, { desc = "Harpoon list" })
+		vim.keymap.set("n", "<C-1>", function()
+			harpoon:list():select(1)
+		end, { desc = "Harpoon file 1" })
+		vim.keymap.set("n", "<C-2>", function()
+			harpoon:list():select(2)
+		end, { desc = "Harpoon file 2" })
+		vim.keymap.set("n", "<C-3>", function()
+			harpoon:list():select(3)
+		end, { desc = "Harpoon file 3" })
+		vim.keymap.set("n", "<C-4>", function()
+			harpoon:list():select(4)
+		end, { desc = "Harpoon file 4" })
+		vim.keymap.set("n", "<C-5>", function()
+			harpoon:list():select(5)
+		end, { desc = "Harpoon file 5" })
+		vim.keymap.set("n", "<C-6>", function()
+			harpoon:list():select(6)
+		end, { desc = "Harpoon file 6" })
+		vim.keymap.set("n", "<C-7>", function()
+			harpoon:list():select(7)
+		end, { desc = "Harpoon file 7" })
+		vim.keymap.set("n", "<C-8>", function()
+			harpoon:list():select(8)
+		end, { desc = "Harpoon file 8" })
+		vim.keymap.set("n", "<C-9>", function()
+			harpoon:list():select(9)
+		end, { desc = "Harpoon file 9" })
+
+		-- hotkeys when harpoon window is open only
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = {
+				"harpoon",
+			},
+			callback = function()
+				vim.keymap.set("n", "1", function()
+					harpoon:list():select(1)
+				end, { buffer = true })
+				vim.keymap.set("n", "2", function()
+					harpoon:list():select(2)
+				end, { buffer = true })
+				vim.keymap.set("n", "3", function()
+					harpoon:list():select(3)
+				end, { buffer = true })
+				vim.keymap.set("n", "4", function()
+					harpoon:list():select(4)
+				end, { buffer = true })
+				vim.keymap.set("n", "5", function()
+					harpoon:list():select(5)
+				end, { buffer = true })
+				vim.keymap.set("n", "6", function()
+					harpoon:list():select(6)
+				end, { buffer = true })
+				vim.keymap.set("n", "7", function()
+					harpoon:list():select(7)
+				end, { buffer = true })
+				vim.keymap.set("n", "8", function()
+					harpoon:list():select(8)
+				end, { buffer = true })
+				vim.keymap.set("n", "9", function()
+					harpoon:list():select(9)
+				end, { buffer = true })
+			end,
+		})
+	end,
 }
--- lua/custom/plugins/rnvimr.lua
+++ lua/custom/plugins/rnvimr.lua
@@ -1,18 +1,14 @@
 return {
-  'kevinhwang91/rnvimr',
-  config = function()
-    -- local rnvimr = require 'rnvimr'
-    -- vim.cmd [[let g:rnvimr_vanilla = 1]]
-    vim.cmd [[let g:rnvimr_enable_picker = 1]]
-    -- vim.cmd [[let g:rnvimr_draw_border = both]]
-    -- vim.cmd [[let g:rnvimr_edit_cmd = 'drop']] -- wtf is this
-    vim.api.nvim_set_keymap('n', '<leader>oo', '', {
-      noremap = true,
-      callback = function()
-        vim.cmd [[RnvimrToggle]]
-        -- rnvimr.RnvimrToggle()
-      end,
-      desc = 'Open Ranger',
-    })
-  end,
+	"kevinhwang91/rnvimr",
+	enabled = false,
+	config = function()
+		vim.cmd([[let g:rnvimr_enable_picker = 1]])
+		vim.api.nvim_set_keymap("n", "<leader>oo", "", {
+			noremap = true,
+			callback = function()
+				vim.cmd([[RnvimrToggle]])
+			end,
+			desc = "Open Ranger",
+		})
+	end,
 }
